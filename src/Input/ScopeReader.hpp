e07782db868c7c882648bf1f1f8474da73668837
Fabian Jung <fabian-11@web.de>
2015-07-29 10:25:44 +0200

    Added Headers for FileReader and ScopeReader

---
+++ src/Input/ScopeReader.hpp
@@ -0,0 +1,18 @@
+#ifndef SCOPEREADER_HPP
+#define SCOPEREADER_HPP
+
+/*
+ *  Based on word done by 2012  Jakub Cizek, Marian Vlcek 
+ * 
+ *  J. Cizek. M. Vlcek, I. Prochazka, Digital spectrometer
+ *  for coincidence measurement of Doppler broadening of positron
+ *  annihilation radiation, Nuclear Instruments and Methods
+ *  in Physics Research, Section A 623, 982-994 (2010)
+ *
+ */
+ 
+ class ScopeReader {
+ 
+ }
+ 
+ #endif
