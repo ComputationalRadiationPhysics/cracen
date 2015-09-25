2a3724d16690ab08f2fea93e6db3f7ef0a6e93d5
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2015-09-25 10:58:34 +0200

    Added Qt Dependencies for OnlineDSP Viewer

---
+++ src/OnlineDSP/OnlineDSP.h
@@ -0,0 +1,37 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2015  <copyright holder> <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef ONLINEDSP_H
+#define ONLINEDSP_H
+
+#include <QtWidgets>
+
+class OnlineDSP : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+OnlineDSP();
+~OnlineDSP();
+virtual QPlatformSurface* surfaceHandle() const;
+
+private:
+};
+
+#endif // ONLINEDSP_H
