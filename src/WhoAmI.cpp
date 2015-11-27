95d97c030dd37f452b3dec1a8802d14894f107c8
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2015-11-27 09:53:49 +0100

    Added Test for the WhoAmI methods

---
+++ src/WhoAmI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Utility/Whoami.hpp"
+
+int main(int argc, char** argv) {
+	if(argc != 2) {
+		std::cout << "Usage: WhoAmI <Remote IP>" << std::endl;
+		return 0;
+	}
+	std::string localIp = Whoami(argv[1]);
+	
+	std::cout << "Local IP " << localIp << std::endl;
+	
+	return 0;
+}
