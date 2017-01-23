4e7d1bdd00e7adc7c47338593ce67e341de448e9
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2017-01-23 16:57:18 +0100

    Added Boost Testing Framework to the project

---
+++ src/Test/CracenUT.cpp
@@ -0,0 +1,11 @@
+#include "Cracen/Cracen.hpp"
+
+#define BOOST_TEST_MODULE CracenTest
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( SourceSinkTest )
+{
+
+
+	BOOST_CHECK( false );
+}
