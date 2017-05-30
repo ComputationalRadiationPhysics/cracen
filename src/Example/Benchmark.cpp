4e3fd7cd7c3da436800136eb257440ebc2087e1e
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2017-05-30 15:52:33 +0200

    Added protocol to Uris

--- src/Example/Benchmark.cpp
+++ src/Example/Benchmark.cpp
@@ -1,6 +1,8 @@
 #include "../Cracen/Cracen.hpp"
 
 #include <boost/mpi.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 #include <array>
 #include <vector>
 #include <cstdint>
@@ -8,8 +10,7 @@
 #include <chrono>
 #include "../graybat/mapping/PeerGroupMapping.hpp"
 #include "../graybat/pattern/Pipeline.hpp"
-
-
+#include "Cracen/Util/Whoami.hpp"
 constexpr unsigned int kilo = 1024;
 constexpr unsigned int mega = kilo * kilo;
 using Chunk = std::vector<char>;
@@ -66,27 +67,30 @@ public:
 	//typedef graybat::communicationPolicy::BMPI CP;
 	typedef graybat::graphPolicy::BGL<unsigned int>    GP;
 	typedef graybat::Cage<CP, GP> Cage;
+	typedef std::pair<std::string, unsigned int> Endpoint;
 
+	const Endpoint signalingIp;
+	const Endpoint localIp ;
 
 	int worldSize;
 	int rank;
-	CageFactory(int worldSize, int rank) :
+	CageFactory(int worldSize, int rank, const Endpoint& signalingIp, const Endpoint& localIp) :
+		signalingIp(signalingIp),
+		localIp(localIp),
 		worldSize(worldSize),
 		rank(rank)
 	{}
 
 	CP::Config commPoly(std::string contextName) {
-		const std::string signalingIp = "127.0.0.1";
-		const std::string localIp = "127.0.0.1";
-		const std::string masterUri = "localhost:"+std::to_string(5000);
-		const std::string peerUri = "tcp://"+localIp+":"+std::to_string(5002);
+
+		const std::string masterUri = "tcp://"+signalingIp.first+":"+std::to_string(signalingIp.second);
+		const std::string peerUri = "tcp://"+localIp.first+":"+std::to_string(localIp.second);
 		//std::cout << "My URI =" << peerUri << std::endl;
 		const unsigned int contextSize = worldSize;
 
 		return CP::Config({masterUri, peerUri, contextSize, contextName}); //ZMQ Config
 		//return CP::Config({}); //BMPI Config
 	}
-
 	auto graphPoly() {
 		unsigned int stageSize = static_cast<int>(worldSize) / 3;
 		std::cout << "Stages:{" << stageSize + (worldSize - 3*stageSize) << ", " << stageSize << ", " << stageSize << "}" << std::endl;
@@ -113,6 +117,23 @@ public:
 };
 
 int main(int argc, char* argv[]) {
+
+	if(argc < 2) {
+		std::cerr << "No configuration file passed. Usage \"mpirun [...] ./Benchmark path/to/config.ini.\"" << std::endl;
+		return 1;
+	}
+
+	boost::property_tree::ptree pt;
+	boost::property_tree::ini_parser::read_ini(argv[1], pt);
+
+	if(pt.get<bool>("local.autoconfiguration")) {
+		std::cout << "Ip autoconfiguration enabled." << std::endl;
+		const std::string local = Whoami(pt.get<std::string>("signaling.ip"));
+		std::cout << "Resolved " << local << std::endl;
+		pt.put<std::string>("local.ip", local);
+	}
+
+
 	std::atomic<bool> done(false);
 	boost::mpi::environment env;
 	boost::mpi::communicator world;
@@ -139,7 +160,15 @@ int main(int argc, char* argv[]) {
 		}
 	});
 
-	CageFactory cf(worldSize, rank);
+	CageFactory::Endpoint signaling = std::make_pair(
+		pt.get<std::string>("signaling.ip"),
+		std::stoul(pt.get<std::string>("signaling.port"), nullptr, 10)
+	);
+	CageFactory::Endpoint local = std::make_pair(
+		pt.get<std::string>("local.ip"),
+		std::stoul(pt.get<std::string>("local.port"), nullptr, 10)
+	);
+	CageFactory cf(worldSize, rank, signaling, local);
 
 	if(rank < stageSize + overhang ) {
 		auto cracen = Cracen::make_cracen(BandwidthSource(), cf, Cracen::BroadCastPolicy());
